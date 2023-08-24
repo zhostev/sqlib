8b7a79fd7da08aa739465080e5c2e25ecf91e2e5
vinsvison <zhaoshijun@yeah.net>
2023-08-24 09:53:27 +0800

    兼容qlib配置格式

--- model_workflow.py
+++ model_workflow.py
@@ -1,47 +1,44 @@
 import yaml
+import pandas as pd
+import mlflow
+from mlflow.entities import Metric
+from mlflow.tracking import MlflowClient
+import plotly.io as pio
+import os
+import tempfile
 from prefect import task, Flow
+from prefect.artifacts import create_link_artifact
 from qlib import init
 import qlib
 from qlib.utils import init_instance_by_config
 from qlib.workflow import R
 from qlib.workflow.record_temp import SignalRecord, PortAnaRecord
-import pandas as pd
-import mlflow
-from mlflow.entities import Metric
-from mlflow.tracking import MlflowClient
 from qlib.data.dataset.handler import DataHandlerLP
-from qlib.contrib.eva.alpha import calc_ic, calc_long_short_return, calc_long_short_prec
+from qlib.contrib.eva.alpha import calc_ic
 from qlib.contrib.strategy import TopkDropoutStrategy
 from qlib.backtest import backtest, executor
-from qlib.utils.time import Freq
 from qlib.contrib.report import analysis_model, analysis_position
 from qlib.contrib.report.analysis_position.report import (
     _report_figure,
     _calculate_report_data,
 )
-from qlib.contrib.report.analysis_position.score_ic import score_ic_graph, _get_score_ic
-import plotly.io as pio
-import os
-import tempfile
+from qlib.contrib.report.analysis_position.score_ic import score_ic_graph
 from qlib.contrib.evaluate import risk_analysis
 from qlib.contrib.data.handler import Alpha158
 from prefect import get_run_logger
-from prefect.artifacts import create_link_artifact
 
 
 @task
 def load_config():
-    with open("config.yaml", "r") as f:
+    with open("workflow_config_lightgbm_Alpha158.yaml", "r") as f:
         config = yaml.safe_load(f)
-    mlflow.set_tracking_uri("http://localhost:5000")
-    mlflow.set_experiment("zhanyuan")
     return config
 
 
 @task
 def init_qlib(config):
-    provider_uri = config["provider_uri"]
-    reg = config["region"]
+    provider_uri = config['qlib_init']["provider_uri"]
+    reg = config['qlib_init']["region"]
     qlib.init(provider_uri=provider_uri, region=reg)
     logger = get_run_logger()
     logger.info("init 初始化成功")
@@ -49,23 +46,26 @@ def init_qlib(config):
 
 @task
 def model_init(config):
-    task = config["model"]
+    task = config["task"]["model"]
     model = init_instance_by_config(task)
     return model
+
 
 @task
 def dataset_init(config):
     data_handler_config = config["data_handler_config"]
     dataset = Alpha158(**data_handler_config)
-    dataset_conf = config["dataset_conf"]
+    dataset_conf = config["task"]["dataset"]
     dataset_conf["kwargs"]["handler"] = dataset
     dataset = init_instance_by_config(dataset_conf)
     return dataset
+
 
 @task
 def train(model, dataset):
     model.fit(dataset)
     return model
+
 
 @task
 def predict(model, dataset):
@@ -76,6 +76,7 @@ def predict(model, dataset):
     label = dataset.prepare(**params)
     return pred, label
 
+
 @task
 def signal_record(pred, label):
     ic, ric = calc_ic(pred.iloc[:, 0], label.iloc[:, 0])
@@ -85,26 +86,31 @@ def signal_record(pred, label):
         mlflow.log_metric("rank_ic", value, step=i)
     return ic, ric
 
+
 @task
 def backtest_record(config, pred):
     FREQ = "day"
-    STRATEGY_CONFIG = config["strategy_config"]
+    STRATEGY_CONFIG = config["port_analysis_config"]["strategy"]['kwargs']
     STRATEGY_CONFIG["signal"] = pred
-    EXECUTOR_CONFIG = config["executor_config"]
-    backtest_config = config["backtest_config"]
+    EXECUTOR_CONFIG = config["port_analysis_config"]["executor"]['kwargs']
+    backtest_config = config["port_analysis_config"]["backtest"]
     strategy_obj = TopkDropoutStrategy(**STRATEGY_CONFIG)
     executor_obj = executor.SimulatorExecutor(**EXECUTOR_CONFIG)
     portfolio_metric_dict, indicator_dict = backtest(
         executor=executor_obj, strategy=strategy_obj, **backtest_config
     )
-    analysis_freq = "{0}{1}".format(*Freq.parse(FREQ))
+    analysis_freq = "{0}{1}".format(*qlib.utils.time.Freq.parse(FREQ))
     report_normal, positions_normal = portfolio_metric_dict.get(analysis_freq)
     report_df = report_normal.copy()
     fig_list = _report_figure(report_df)
     return report_df, fig_list
 
+
 @Flow
-def run_workflow(name="sqlib_workflow"):
+def run_workflow(name="qlib_workflow"):
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_experiment("zhanyuan")
+
     with mlflow.start_run() as run:
         config = load_config()
         mlflow.lightgbm.autolog()
@@ -113,14 +119,13 @@ def run_workflow(name="sqlib_workflow"):
         dataset = dataset_init(config)
         model = train(model, dataset)
         pred, label = predict(model, dataset)
-        ic, ric = signal_record(pred, label)
+        # ic, ric = signal_record(pred, label)
         report_df, fig_list = backtest_record(config, pred)
-
-
 
 
 def main():
     run_workflow()
-    
+
+
 if __name__ == "__main__":
     main()
