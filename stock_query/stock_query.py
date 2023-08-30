35ce195f8635b92f08eb2d9768277479a4a1bd76
vinsvison <zhaoshijun@yeah.net>
2023-08-30 23:01:28 +0800

    增加一个新的选股方式：爱问财，可以有效的解决股票池的问题

---
+++ stock_query/stock_query.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import requests
+
+
+class StockQuery:
+    def __init__(self):
+        self.url = "https://www.iwencai.com/gateway/urp/v7/landing/getDataList"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+        }
+
+    def query_stock(self, query: str) -> pd.DataFrame:
+        params = {
+            "query": query,
+            "urp_sort_way": "desc",
+            "urp_sort_index": "",
+            "page": "1",
+            "perpage": "100",
+            "addheaderindexes": "",
+            "condition": "",
+            "codelist": "",
+            "indexnamelimit": "",
+            "ret": "json_all",
+            "source": "Ths_iwencai_Xuangu",
+            "urp_use_sort": "1",
+            "uuids[0]": "24087",
+            "query_type": "stock",
+            "comp_id": "6836372",
+            "business_cat": "soniu",
+            "uuid": "24087",
+        }
+        response = requests.get(self.url, params=params, headers=self.headers)
+        data_json = response.json()
+        result_df = pd.DataFrame(data_json["answer"]["components"][0]["data"]["datas"])
+        return result_df
+
+
+
+
+if __name__ == "__main__":
+    stock_query = StockQuery()
+    result = stock_query.query_stock("热度前十的股票")
+    print(result)
